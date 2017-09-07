1ba6a0cee661b80ad7fd0826b7a34d7d0b3134da
lukney <sangeetha18lukney@gmail.com>
2017-09-07 02:20:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
+//start mysql connection
+var connection = mysql.createConnection({
+  host     : 'localhost', //mysql database host name
+  user     : 'root', //mysql database user name
+  password : '', //mysql database password
+  database : 'courier' //mysql database name
+});
+
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected with mysql database...')
+})
+//end mysql connection
+
+//start body-parser configuration
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+//end body-parser configuration
+
+//create app server
+var server = app.listen(3005,   function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
+
+//rest api to get all customers
+app.get('/customer', function (req, res) {
+   connection.query('select * from karnataka', function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
