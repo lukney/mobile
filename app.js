314d559c8438e52e94ed3d4c93591baf61ca15c1
lukney <sangeetha18lukney@gmail.com>
2017-09-08 04:07:56 -0700

    file chan

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser');
 
 
 var connection = mysql.createConnection({
-  host     : 'mitali.cgcpmi6mcuyv.us-east-2.rds.amazonaws.com',
-  user     : 'mitali',
+  host     : 'sangeetha.cgcpmi6mcuyv.us-east-2.rds.amazonaws.com',
+  user     : 'sangeetha',
   password : '12345678',
   port     : '3306',
-database : 'mitali'
+database : 'sangeetha'
 });
 
 connection.connect(function(err) {
@@ -21,7 +21,7 @@ connection.connect(function(err) {
   }
 
  else{ console.log('Connected to database.');
-connection.query("SELECT * FROM new_table", function (err, result, fields) {
+connection.query("SELECT * FROM karnataka", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
 }
@@ -33,9 +33,9 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-var server = app.listen(3009,   function () {
+var server = app.listen(3001,   function () {
 
-  var host = 'ec2-18-220-142-169.us-east-2.compute.amazonaws.com'
+  var host = 'ec2-18-221-18-150.us-east-2.compute.amazonaws.com'
   var port = server.address().port
 
   console.log("Example app listening at http://%s:%s", host, port)
@@ -44,7 +44,7 @@ var server = app.listen(3009,   function () {
 
 //rest api to get all customers
 app.get('/customer', function (req, res) {
-   connection.query('select * from new_table', function (error, results, fields) {
+   connection.query('select * from karnataka', function (error, results, fields) {
 	  if (error) throw error;
 	  res.end(JSON.stringify(results));
 	});
